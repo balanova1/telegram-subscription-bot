87ef07006854f23fd38748e2c2fa7bacc02d1c23
balanova1 <sasha.balanova@gmail.com>
2026-01-20 20:27:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def start(message):
         member = bot.get_chat_member(CHANNEL, user_id)
 
         if member.status in ["member", "administrator", "creator"]:
-           with open("guide.pptx.pdf", "rb") as file:
+           with open("guide1.pptx.pdf", "rb") as file:
                 bot.send_document(
                 message.chat.id,
                 file,
